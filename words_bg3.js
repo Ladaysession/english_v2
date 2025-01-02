6f5cd56afe3ac1b6a4336ea523760f8daf16bda9
Ladaysession <116974378+Ladaysession@users.noreply.github.com>
2025-01-02 06:27:35 +0200

    Add files via upload

---
+++ words_bg3.js
@@ -0,0 +1,233 @@
+let Words = {
+    "0": [
+        {
+            "english": "Unnerving",
+            "translate": "Нервирует",
+            "image": [
+                {
+                    "image_path": "https://images.unsplash.com/photo-1570073074941-0ad29c8c1345?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2ODIwNTl8MHwxfHNlYXJjaHwxfHxVbm5lcnZpbmd8ZW58MHx8fHwxNzM1NzgyMjMxfDA&ixlib=rb-4.0.3&q=80&w=1080"
+                }
+            ],
+            "example": []
+        },
+        {
+            "english": "Erode",
+            "translate": "Разъедать",
+            "image": [
+                {
+                    "image_path": "https://static1.squarespace.com/static/54ad91eae4b04d2abc8d6247/t/56cd3dd9a3360cdd18dc99c9/1456291293508/?format=1500w"
+                }
+            ],
+            "example": []
+        },
+        {
+            "english": "upbringing",
+            "translate": "воспитание",
+            "image": [],
+            "example": [
+                {
+                    "example_path": "She had a strict upbringing that shaped her disciplined character."
+                },
+                {
+                    "example_path": "У неё было строгое воспитание, которое сформировало её дисциплинированный характер."
+                }
+            ]
+        },
+        {
+            "english": "resilient",
+            "translate": "стойкий, устойчивый",
+            "image": [
+                {
+                    "image_path": "https://images.unsplash.com/photo-1526444993807-89612851ea2d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2ODIwNTl8MHwxfHNlYXJjaHwxfHxyZXNpbGllbnR8ZW58MHx8fHwxNzM1NzgyOTcxfDA&ixlib=rb-4.0.3&q=80&w=1080"
+                }
+            ],
+            "example": [
+                {
+                    "example_path": "Children are incredibly resilient when faced with challenges."
+                }
+            ]
+        },
+        {
+            "english": "taught",
+            "translate": "обученный, научил (прошедшее время от teach)",
+            "image": [
+                {
+                    "image_path": "https://images.unsplash.com/photo-1541018939203-36eeab6d5721?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2ODIwNTl8MHwxfHNlYXJjaHwxfHx0YXVnaHR8ZW58MHx8fHwxNzM1NzgyOTg1fDA&ixlib=rb-4.0.3&q=80&w=1080"
+                }
+            ],
+            "example": [
+                {
+                    "example_path": "She taught her students to think critically and independently."
+                }
+            ]
+        },
+        {
+            "english": "perils",
+            "translate": "опасности, риски",
+            "image": [
+                {
+                    "image_path": "https://images.unsplash.com/photo-1536228035407-534ae8022bd6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2ODIwNTl8MHwxfHNlYXJjaHwxfHxwZXJpbHN8ZW58MHx8fHwxNzM1NzgyOTk1fDA&ixlib=rb-4.0.3&q=80&w=1080"
+                }
+            ],
+            "example": [
+                {
+                    "example_path": "The adventurers were well aware of the perils they might encounter."
+                }
+            ]
+        },
+        {
+            "english": "fuss",
+            "translate": "суета, шум, возмущение",
+            "image": [
+                {
+                    "image_path": "https://images.unsplash.com/photo-1672073843195-64a84386137d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2ODIwNTl8MHwxfHNlYXJjaHwxfHxmdXNzfGVufDB8fHx8MTczNTc4MzEyM3ww&ixlib=rb-4.0.3&q=80&w=1080"
+                }
+            ],
+            "example": [
+                {
+                    "example_path": "She made such a fuss over a small mistake."
+                }
+            ]
+        },
+        {
+            "english": "gory",
+            "translate": "кровавый, жестокий",
+            "image": [
+                {
+                    "image_path": "https://smartvocab.in/vocab-api/common-images/28ocofkbunseopci.jpg?v=1"
+                }
+            ],
+            "example": [
+                {
+                    "example_path": "The movie was too gory for me to watch."
+                }
+            ]
+        },
+        {
+            "english": "roving",
+            "translate": "странствующий, блуждающий",
+            "image": [
+                {
+                    "image_path": "https://live.staticflickr.com/6203/6116411329_98a8502229_b.jpg"
+                }
+            ],
+            "example": [
+                {
+                    "example_path": "He spent years roving across the countryside in search of inspiration."
+                }
+            ]
+        },
+        {
+            "english": "wit",
+            "translate": "остроумие, ум",
+            "image": [
+                {
+                    "image_path": "https://images.unsplash.com/photo-1712493204781-e3000976dd07?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2ODIwNTl8MHwxfHNlYXJjaHwxfHx3aXR8ZW58MHx8fHwxNzM1NzgzODAwfDA&ixlib=rb-4.0.3&q=80&w=1080"
+                }
+            ],
+            "example": [
+                {
+                    "example_path": "Her quick wit made her the star of every conversation."
+                }
+            ]
+        },
+        {
+            "english": "scimitar",
+            "translate": "ятаган, изогнутый восточный меч",
+            "image": [
+                {
+                    "image_path": "https://cdn.reliks.com/products/2168/1200x0/scimitar.jpg"
+                }
+            ],
+            "example": [
+                {
+                    "example_path": "The warrior brandished his scimitar, ready for battle."
+                }
+            ]
+        },
+        {
+            "english": "timber",
+            "translate": "древесина, лесоматериалы",
+            "image": [
+                {
+                    "image_path": "https://images.unsplash.com/photo-1590880795696-20c7dfadacde?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2ODIwNTl8MHwxfHNlYXJjaHwxfHx0aW1iZXJ8ZW58MHx8fHwxNzM1Nzg0Mzg5fDA&ixlib=rb-4.0.3&q=80&w=1080"
+                }
+            ],
+            "example": [
+                {
+                    "example_path": "The carpenter used high-quality timber to build the house."
+                }
+            ]
+        },
+        {
+            "english": "ripe",
+            "translate": "спелый, зрелый",
+            "image": [
+                {
+                    "image_path": "https://images.unsplash.com/photo-1503327655231-9a047d4772b6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2ODIwNTl8MHwxfHNlYXJjaHwxfHxyaXBlfGVufDB8fHx8MTczNTc4NDc2MHww&ixlib=rb-4.0.3&q=80&w=1080"
+                }
+            ],
+            "example": [
+                {
+                    "example_path": "The tomatoes are ripe and ready to be picked."
+                }
+            ]
+        },
+        {
+            "english": "circus",
+            "translate": "цирк",
+            "image": [
+                {
+                    "image_path": "https://images.unsplash.com/photo-1435034568314-8303dbda4b8c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2ODIwNTl8MHwxfHNlYXJjaHwxfHxjaXJjdXN8ZW58MHx8fHwxNzM1Nzg0ODQwfDA&ixlib=rb-4.0.3&q=80&w=1080"
+                }
+            ],
+            "example": [
+                {
+                    "example_path": "The children were thrilled to see the elephants at the circus."
+                }
+            ]
+        },
+        {
+            "english": "reed",
+            "translate": "тростник, камыш",
+            "image": [
+                {
+                    "image_path": "https://images.unsplash.com/photo-1424786194759-3e6b91d8d3bb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2ODIwNTl8MHwxfHNlYXJjaHwxfHxyZWVkfGVufDB8fHx8MTczNTc4NzQyMnww&ixlib=rb-4.0.3&q=80&w=1080"
+                }
+            ],
+            "example": [
+                {
+                    "example_path": "The wind rustled gently through the reed by the riverbank."
+                }
+            ]
+        },
+        {
+            "english": "scurry",
+            "translate": "суетиться, спешить",
+            "image": [
+                {
+                    "image_path": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS27GnOKNILJB4EQg97zmCMpqW6wCaT6fz9Hw&s"
+                }
+            ],
+            "example": [
+                {
+                    "example_path": "The mice scurry across the floor when the lights turn on."
+                }
+            ]
+        },
+        {
+            "english": "incense",
+            "translate": "возмущать, сердить; кадить, окуривать благовониями",
+            "image": [
+                {
+                    "image_path": "https://images.unsplash.com/photo-1611800065908-233b597db552?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2ODIwNTl8MHwxfHNlYXJjaHwxfHxpbmNlbnNlfGVufDB8fHx8MTczNTc4ODU5OHww&ixlib=rb-4.0.3&q=80&w=1080"
+                }
+            ],
+            "example": [
+                {
+                    "example_path": "The rude comment seemed to incense everyone in the room."
+                }
+            ]
+        }
+    ]
+};
